4b456e237a2acd3c663c7266e1eabfafe135d768
alabzin <kard33@mail.ru>
2024-12-09 10:52:36 +0300

    init commit

--- src/test/java/drivers/BrowserstackDriver.java
+++ src/test/java/drivers/BrowserstackDriver.java
@@ -1,53 +1,49 @@
 package drivers;
 
 import com.codeborne.selenide.WebDriverProvider;
-import config.TestConfigAndroid;
-import config.TestConfigIOS;
+import config.WebDriverConfig;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import javax.annotation.Nonnull;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
 
 
 public class BrowserstackDriver implements WebDriverProvider {
 
+    private final WebDriverConfig config;
+
+    public BrowserstackDriver() {
+        this.config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+    }
+
+    @Nonnull
     @Override
-    public WebDriver createDriver(Capabilities capabilities) {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.merge(capabilities);
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+        MutableCapabilities caps = new MutableCapabilities();
 
-        String platform = System.getProperty("platform", "android");
+        // Set your access credentials
+        caps.setCapability("browserstack.user", config.getBrowserstackUser());
+        caps.setCapability("browserstack.key", config.getBrowserstackKey());
 
-        if ("ios".equalsIgnoreCase(platform)) {
-            TestConfigIOS iosConfig = ConfigFactory.create(TestConfigIOS.class);
-            caps.setCapability("appium:deviceName", iosConfig.device());
-            caps.setCapability("appium:osVersion", iosConfig.osVersion());
-            caps.setCapability("appium:app", iosConfig.appUrl());
-            caps.setCapability("bstack:options", new HashMap<String, Object>() {{
-                put("userName", iosConfig.browserstackUser());
-                put("accessKey", iosConfig.browserstackKey());
-            }});
-        } else {
-            TestConfigAndroid androidConfig = ConfigFactory.create(TestConfigAndroid.class);
-            caps.setCapability("appium:deviceName", androidConfig.device());
-            caps.setCapability("appium:osVersion", androidConfig.osVersion());
-            caps.setCapability("appium:app", androidConfig.appUrl());
-            caps.setCapability("bstack:options", new HashMap<String, Object>() {{
-                put("userName", androidConfig.browserstackUser());
-                put("accessKey", androidConfig.browserstackKey());
-            }});
-        }
+        // Set URL of the application under test
+        caps.setCapability("app", config.getApp());
 
+        // Specify device and os_version for testing
+        caps.setCapability("device", config.getDevice());
+        caps.setCapability("os_version", config.getOsVersion());
+
+        // Initialise the remote Webdriver using BrowserStack remote URL
+        // and desired capabilities defined above
         try {
-            return new RemoteWebDriver(new URL(System.getProperty("browserstack.url")), caps);
+            return new RemoteWebDriver(
+                    new URL(config.getRemoteUrl()), caps);
         } catch (MalformedURLException e) {
-            throw new RuntimeException("Invalid BrowserStack URL", e);
+            throw new RuntimeException(e);
         }
     }
 }
